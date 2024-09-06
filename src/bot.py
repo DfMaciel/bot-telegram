9634ae51adc2d48d0c6ac53c284d82de3b9dbf69
Davi Maciel Ferreira <104574671+DfMaciel@users.noreply.github.com>
2024-09-06 10:07:49 -0300

    Tokens gerados

---
+++ src/bot.py
@@ -0,0 +1,8 @@
+#  telegram 7445994394:AAEUkcb7dJxDofhTZ6bbwlzP9g6IOTUJ-yY
+
+# Api AIzaSyCUfCjcTx35Hjs4vcAImh_ZvgPSseEsOgc
+
+# script async src="https://cse.google.com/cse.js?cx=46baa85e7b33e4b03">
+# </script>
+# <div class="gcse-search"></div> 
+
